c4aedc14568b1a07350f8f1cc74c73052c231093
Mayank Mishra <maymishraank@gmail.com>
2022-01-17 21:56:45 +0530

    Changed port

--- app.js
+++ app.js
@@ -32,6 +32,6 @@ app.use((error, req, res, next) => {
 
 connectDB();
 
-app.listen(5000, () => {
-  console.log("Listening on port 3000");
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Listening on port 5000");
 });
